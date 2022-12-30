581f129ddd92b91b746b494deee036ad971db1fc
Aaron <Harvey.AaronJ@gmail.com>
2022-12-30 12:07:24 -0500

    Builds helloWorld.js - writes change Handler function

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,5 +1,8 @@
 import { LightningElement } from 'lwc';
 
 export default class HelloWorld extends LightningElement {
-   
+	greeting = 'World';
+	changeHandler (e) {
+		this.greeting = e.target.value;
+	}
 }
